798834b04b9e042171eb924c21e73f4b2f35ac60
Guilherme P. Garcia <guip.garcia@gmail.com>
2023-04-01 20:23:46 +0100

    updating the commands and add new variables to avoid the hardcoded information

---
+++ cypress/support/VariablesDefaultValues.js
@@ -0,0 +1,24 @@
+/// <reference types = 'Cypress'/>
+
+class VariablesDefaultValues{
+     variables =
+     {      
+            "delay" : 200,    
+            "not" : "not.",
+            "firstName" : 'Some Name', 
+            "lastName" : 'Some LastName', 
+            "email" : 'somemail@mail.com', 
+            "openTextArea" : 'Inserting some text just to fill this blooding field',
+            "phone" : '123456789',
+            "wrongEmailFormat": "email",
+            "phoneJustWords": "phone",
+            "someLetters": "abcdef",
+            "youtubeOption": "YouTube",
+            "mentoriaOption": "Mentoria",
+            "cursosOption": "Cursos",
+            "ajudaOption" : "Ajuda",
+            "emailCheckbox" : "Email",
+            "phoneCheckbox": "Phone"
+    };
+}
+export default VariablesDefaultValues
